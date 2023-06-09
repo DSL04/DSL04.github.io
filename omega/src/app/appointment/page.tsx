fb4eb1ff60533683f8f5bb01669b0c133b4a6cd3
DSL04 <souit02@outlook.com>
2023-06-09 01:24:00 -0400

    Tasl #5 : Semi-complete
    - finished designing services and appointments page based on figma design
    - once again, mobile responsiveness is a working progress

--- omega/src/app/appointment/page.tsx
+++ omega/src/app/appointment/page.tsx
@@ -10,13 +10,68 @@ export default function Appointment() {
             </div>
 
             {/* The services */}
-            <div className="grid grid-cols-2 grid-rows-3 gap-3 justify-items-center">
-                <div className="">Item 1</div>
-                <div className="grid-item">Item 2</div>
-                <div className="grid-item">Item 3</div>
-                <div className="grid-item">Item 4</div>
-                <div className="grid-item">Item 5</div>
-                <div className="grid-item">Item 6</div>
+            <div className="grid grid-cols-2 grid-rows-3 gap-3 justify-items-center mt-5">
+                <div className="">
+                    <div className=''><img className='imageStyling w-72' src='https://www.byrdie.com/thmb/bPRFkPqEM6g6ZgsU2mVdBcgQQxs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/120204360_4253312141364853_6255456248061584670_n-26cbf0037a4a4ca7ba559d969990edaf.jpg' /></div>
+                    <div className=' space-y-8'>
+                        <div className=' flex mt-4'>
+                            <div className='serviceText flex justify-start min-w-max'>FULL CUT</div>
+                            <div className='serviceText flex justify-end'>28$</div>
+                        </div>
+                        <div className=' cutDescriptionText w-72 flex justify-center'> A COMPLETE HAIRCUT THAT INVOLVES THE BEARD</div>
+                        <div className=' serviceText flex justify-center'><button className='button button3'>SELECT CUT</button></div>
+                    </div>
+                </div>
+                
+                <div className="">
+                    <div className=''><img className='imageStyling w-72 h-72' src='https://i.pinimg.com/564x/29/ac/7c/29ac7c05e9f34738cb5f917383b449f8.jpg' /></div>
+                    <div className=' space-y-8'>
+                        <div className=' flex mt-4'>
+                            <div className='serviceText flex justify-start min-w-max'>BEARD JOB</div>
+                            <div className='serviceText flex justify-end'>15$</div>
+                        </div>
+                        <div className=' cutDescriptionText w-72 flex justify-center'> TRIMMING, SHAPING, AND GROOMING OF FACIAL HAIR</div>
+                        <div className=' serviceText flex justify-center'><button className='button button3'>SELECT CUT</button></div>
+                    </div>
+                </div>
+                
+                <div className="">
+                    <div className=''><img className='imageStyling w-72 h-72 align-top' src='https://www.menshairstyletrends.com/wp-content/uploads/2020/07/Mexican-taper-fade-haircut-omithebarber_-820x1024.jpg' /></div>
+                    <div className=' space-y-8'>
+                        <div className=' flex mt-4'>
+                            <div className='serviceText flex justify-start min-w-max'>TAPER</div>
+                            <div className='serviceText flex justify-end'>20$</div>
+                        </div>
+                        <div className=' cutDescriptionText w-72 flex justify-center'> GRADUALLY DECREASING THE LENGTH OF THE HAIR FROM THE TOP TO THE SIDES AND BACK</div>
+                        <div className=' serviceText flex justify-center'><button className='button button3'>SELECT CUT</button></div>
+                    </div>
+                </div>
+                
+                <div className="">
+                    <div className=''><img className='imageStyling w-72' src='https://i.pinimg.com/564x/5c/c8/0e/5cc80e763c515beb07830f31fabe36a2.jpg' /></div>
+                    <div className=' space-y-8'>
+                        <div className=' flex mt-4'>
+                            <div className='serviceText flex justify-start min-w-max'>KIDS CUT(14)</div>
+                            <div className='serviceText flex justify-end'>22$</div>
+                        </div>
+                        <div className=' cutDescriptionText w-72 flex justify-center'> FULL CUT FOR CHILDREN. ONLY KIDS BELOW THE AGE OF 15 YEARS OF AGE ARE ELIGIBLE FOR THIS CUT</div>
+                        <div className=' serviceText flex justify-center'><button className='button button3'>SELECT CUT</button></div>
+                    </div>
+                </div>
+                
+                <div className="mt-6">
+                    <div className=''><img className='imageStyling lineupSwitch w-72' src='https://i.pinimg.com/564x/cc/46/6e/cc466e1cd315442ff3ce853ef6525c7d.jpg' /></div>
+                    <div className=' space-y-8'>
+                        <div className=' flex mt-4'>
+                            <div className='serviceText flex justify-start min-w-max'>LINE-UP</div>
+                            <div className='serviceText flex justify-end'>10$</div>
+                        </div>
+                        <div className=' cutDescriptionText w-72 flex justify-center'> SHAPING AND DEFINING OF THE HAIRLINE AND EDGES</div>
+                        <div className=' serviceText flex justify-center'><button className='button button3'>SELECT CUT</button></div>
+                    </div>
+                </div>
+                
+                {/* <div className="grid-item">Item 6</div> */}
             </div>
 
 
--- omega/src/app/team/page.tsx
+++ omega/src/app/team/page.tsx
@@ -16,23 +16,25 @@ export default function team() {
                     <div className=''>
                         <div className=' flex justify-around'><h1 className='barberTitle'>OTTAWA BARBER</h1></div>
                         
-                        <div className=' flex justify-evenly mt-5'>
-                            <div className=' ottawaBarberImage'><img/></div>
-                            <div className=' w-1/3 flex-wrap'><p className=' break-words text'>
-                                Meet Hikaru Smith, a talented barber based in the charming city of Ottawa. Hikaru's journey in the world of barbering started at a young age, inspired by his family's legacy of hairstyling. With his keen eye for creativity and dedication to customer satisfaction, 
-                                Hikaru has built a loyal clientele in Ottawa's diverse community. His modern approach and commitment to delivering top-notch grooming experiences make him a sought-after barber in the capital city.</p>
-                             </div>
-                        </div>
-
-                        <div className=' flex justify-evenly mt-60 mb-3'>
-                            <div className=' w-1/3 flex-wrap'>
-                              <ul className='ul'>
-                                <li className='li'>Recipient of the "Rising Star Barber" award, Ottawa Barber Association, 2022</li>
-                                <li className='li'>Featured in Ottawa Style Magazine for his innovative hairstyling techniques</li>
-                                <li className='li'>Official barber for local fashion shows and editorial photoshoots</li>
-                              </ul>  
+                        <div className=' space-y-60'>
+                            <div className=' flex justify-evenly mt-5'>
+                                <div className=' ottawaBarberImage'><img/></div>
+                                <div className=' w-1/3 flex-wrap'><p className=' break-words text'>
+                                    Meet Hikaru Smith, a talented barber based in the charming city of Ottawa. Hikaru's journey in the world of barbering started at a young age, inspired by his family's legacy of hairstyling. With his keen eye for creativity and dedication to customer satisfaction, 
+                                    Hikaru has built a loyal clientele in Ottawa's diverse community. His modern approach and commitment to delivering top-notch grooming experiences make him a sought-after barber in the capital city.</p>
+                                </div>
                             </div>
-                            <div className=' ottawaBarberAward'><img/></div>  
+
+                            <div className=' flex justify-evenly mb-3'>
+                                <div className=' w-1/3 flex-wrap'>
+                                <ul className='ul space-y-10'>
+                                    <li className='li'>Recipient of the "Rising Star Barber" award, Ottawa Barber Association, 2022</li>
+                                    <li className='li'>Featured in Ottawa Style Magazine for his innovative hairstyling techniques</li>
+                                    <li className='li'>Official barber for local fashion shows and editorial photoshoots</li>
+                                </ul>  
+                                </div>
+                                <div className=' ottawaBarberAward'><img/></div>  
+                            </div>
                         </div>
                     </div>
 
@@ -41,27 +43,29 @@ export default function team() {
                     <div className=''>
                         <div className=' flex justify-around'><h1 className='barberTitle'>TORONTO BARBER</h1></div>
                         
-                        <div className=' flex justify-evenly mt-5'>
-                            <div className=' torontoBarberImage'><img/></div>
-                            <div className=' w-1/3 flex-wrap'><p className=' break-words text'>With a passion for precision and style, Joe Boe is a renowned barber hailing from the vibrant city of Toronto. With several years of experience in the industry, 
-                            Joe's expertise lies in creating modern and trend-setting hairstyles for his clients. Known for his attention to detail and friendly demeanor, Joe Boe has become a trusted name in Toronto's grooming scene.</p>
-                             </div>
-                        </div>
-
-                        <div className=' flex justify-evenly mt-60 mb-3'>
-                            <div className=' w-1/3 flex-wrap'>
-                              <ul className='ul'>
-                                <li className=''>Winner of the "Best Barber in Toronto" award, 2021</li>
-                                <li className='li'>Featured in Men's Grooming Magazine for his exceptional razor techniques</li>
-                                <li className='li'>Official barber for various Toronto-based celebrities and influencers</li>
-                              </ul>  
+                        <div className=' space-y-60'>
+                            <div className=' flex justify-evenly mt-5'>
+                                <div className=' torontoBarberImage'><img/></div>
+                                <div className=' w-1/3 flex-wrap'><p className=' break-words text'>With a passion for precision and style, Joe Boe is a renowned barber hailing from the vibrant city of Toronto. With several years of experience in the industry, 
+                                Joe's expertise lies in creating modern and trend-setting hairstyles for his clients. Known for his attention to detail and friendly demeanor, Joe Boe has become a trusted name in Toronto's grooming scene.</p>
+                                </div>
                             </div>
-                            <div className=' ottawaBarberAward'><img/></div>  
+
+                            <div className=' flex justify-evenly mb-3'>
+                                <div className=' w-1/3 flex-wrap'>
+                                <ul className='ul space-y-10'>
+                                    <li className='li'>Winner of the "Best Barber in Toronto" award, 2021</li>
+                                    <li className='li'>Featured in Men's Grooming Magazine for his exceptional razor techniques</li>
+                                    <li className='li'>Official barber for various Toronto-based celebrities and influencers</li>
+                                </ul>  
+                                </div>
+                                <div className=' ottawaBarberAward'><img/></div>  
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='ml-2 mt-10 mb-10 flex-wrap'><h1 className='text'>NOTHING IS OUTSIDE OF THEIR REACH. FROM CLASSIC CUTS TO DARING TRANSFORMATIONS, EVERY STRAND BECOMES A TESTAMENT TO THEIR BOUNDLESS CREATIVITY.</h1></div>
+            <div className='ml-2 mt-10 mb-10 flex-wrap flex'><h1 className='text'>NOTHING IS OUTSIDE OF THEIR REACH. FROM CLASSIC CUTS TO DARING TRANSFORMATIONS, EVERY STRAND BECOMES A TESTAMENT TO THEIR BOUNDLESS CREATIVITY.</h1></div>
         </div>
 
     )
